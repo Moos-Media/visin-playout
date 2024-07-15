d481684cbc1ef8bb8fd98ffb6c19a7cc9bf7c522
Lukas Moos <lukas.stephan.moos@muk.thm.de>
2024-07-15 10:10:25 +0200

    Added remote Brightness Update.

--- src/main.cpp
+++ src/main.cpp
@@ -41,16 +41,18 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 const char *ssid = "ESP32-Light-Server";
 const char *password = "adminLukas";
 const char *serverName = "http://192.168.4.2:8001/api/show/getCurrentBoard";
+const char *brightnessURL = "http://192.168.4.2:8001/api/show/getBrightness";
 WiFiClient client;
 HTTPClient http;
+int frameCounter = 0;
 
 // setup() function -- runs once at startup --------------------------------
 void setup()
 {
   Serial.begin(115200);
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.begin();            // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();             // Turn OFF all pixels ASAP
+  strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
 
   WiFi.softAP(ssid, password);
   // Send request
@@ -61,56 +63,86 @@ void setup()
 // loop() function -- runs repeatedly as long as board is on ---------------
 void loop()
 {
-  http.POST("");
+  int responseCode = http.POST("");
 
-  // Parse response
-  DynamicJsonDocument doc(2048);
-  deserializeJson(doc, http.getStream());
-
-  strip.clear();
-  for (int i = 0; i < LED_COUNT; i++)
+  if (responseCode == 200)
   {
-    String colorName = doc["boardInfo"]["board"][i]["color"].as<String>();
+    // Parse response
+    DynamicJsonDocument doc(2048);
+    deserializeJson(doc, http.getStream());
 
-    // Conected LEDs in demonstration unit is wired wrongly.
-    // All Colors are sent as GRB instead of RGB.
-    if (colorName == "WHITE")
+    strip.clear();
+    for (int i = 0; i < LED_COUNT; i++)
     {
-      strip.setPixelColor(i, 255, 255, 255);
+      String colorName = doc["boardInfo"]["board"][i]["color"].as<String>();
+
+      // Conected LEDs in demonstration unit is wired wrongly.
+      // All Colors are sent as GRB instead of RGB.
+      if (colorName == "WHITE")
+      {
+        strip.setPixelColor(i, 255, 255, 255);
+      }
+      else if (colorName == "GREEN")
+      {
+        strip.setPixelColor(i, 186, 128, 36);
+      }
+      else if (colorName == "COLOR1")
+      {
+        strip.setPixelColor(i, 107, 24, 162);
+      }
+      else if (colorName == "COLOR2")
+      {
+        strip.setPixelColor(i, 17, 192, 17);
+      }
+      else if (colorName == "COLOR3")
+      {
+        strip.setPixelColor(i, 178, 223, 52);
+      }
+      else if (colorName == "COLOR4")
+      {
+        strip.setPixelColor(i, 49, 216, 127);
+      }
+      else if (colorName == "COLOR5")
+      {
+        strip.setPixelColor(i, 102, 230, 47);
+      }
+      else if (colorName == "COLOR6")
+      {
+        strip.setPixelColor(i, 0, 184, 172);
+      }
     }
-    else if (colorName == "GREEN")
-    {
-      strip.setPixelColor(i, 186, 128, 36);
-    }
-    else if (colorName == "COLOR1")
-    {
-      strip.setPixelColor(i, 107, 24, 162);
-    }
-    else if (colorName == "COLOR2")
-    {
-      strip.setPixelColor(i, 17, 192, 17);
-    }
-    else if (colorName == "COLOR3")
-    {
-      strip.setPixelColor(i, 178, 223, 52);
-    }
-    else if (colorName == "COLOR4")
-    {
-      strip.setPixelColor(i, 49, 216, 127);
-    }
-    else if (colorName == "COLOR5")
-    {
-      strip.setPixelColor(i, 102, 230, 47);
-    }
-    else if (colorName == "COLOR6")
-    {
-      strip.setPixelColor(i, 0, 184, 172);
-    }
+
+    strip.show();
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000 / FRAMERATE));
+
+    // Disconnect
+    http.end();
   }
 
-  strip.show();
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000 / FRAMERATE));
+  frameCounter += 1;
 
-  // Disconnect
-  http.end();
+  if (frameCounter > 200)
+  {
+    // End old connection
+    frameCounter = 0;
+    http.end();
+
+    // Connect to new URL
+    http.begin(client, brightnessURL);
+
+    int responseCode = http.POST("");
+
+    if (responseCode == 200)
+    {
+      // Parse response
+      DynamicJsonDocument doc(2048);
+      deserializeJson(doc, http.getStream());
+
+      int newBrightness = doc["brightness"].as<int>();
+      strip.setBrightness(newBrightness);
+    }
+
+    http.end();
+    http.begin(client, serverName);
+  }
 }
